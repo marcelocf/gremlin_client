f6ee9926d8906e1259412872ca3ac6829edf839a
フレイタス　マルセロ <marcelo.freitas@finc.com>
2017-02-28 17:06:17 +0900

    fix includes

--- lib/gremlin_client.rb
+++ lib/gremlin_client.rb
@@ -1,9 +1,8 @@
 # Module encapsulating our code
 
-require 'json'
 require 'rubygems'
+require 'json'
 require 'websocket-client-simple'
-require 'faye/websocket'
 
 require 'gremlin_client/server_error'
 require 'gremlin_client/execution_timeout_error'
