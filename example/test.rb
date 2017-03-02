752287fbaaad11cc283189d335201470c727c682
フレイタス　マルセロ <marcelo.freitas@finc.com>
2017-03-02 09:23:41 +0900

    fix example syntax

--- example/test.rb
+++ example/test.rb
@@ -4,11 +4,11 @@ require 'pp'
 require 'gremlin_client'
 
 
-conn = GremlinClient::Connection.new(groovy_script_path: 'example/scripts')
+conn = GremlinClient::Connection.new(gremlin_script_path: 'example/scripts')
 
-pp conn.send('1+what', {what: 10})
+pp conn.send_query('1+what', {what: 10})
 
-pp conn.send('g.V().count()')
+pp conn.send_query('0 && 1')
 
 pp conn.send_file('test.groovy', {what: Time.now.to_i})
 
