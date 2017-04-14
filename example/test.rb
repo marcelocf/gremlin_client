b3c94f08dbee5f74ba44f6b7cfe80022feaf9a3c
フレイタス　マルセロ <marcelo.freitas@finc.com>
2017-04-14 14:28:27 +0900

    fix test

--- example/test.rb
+++ example/test.rb
@@ -5,26 +5,4 @@ require 'gremlin_client'
 
 
 conn = GremlinClient::Connection.new(gremlin_script_path: 'example/scripts')
-
-
-def build_message(command, bindings)
-  message = {
-    requestId: 1231231,
-    op: 'eval',
-    processor: '',
-    args: {
-      gremlin: command,
-      bindings: bindings,
-      language: 'gremlin-groovy'
-    }
-  }
-end
-
-
-msg =build_message('some big string', { lala: 123, lolo: [1,2,3]})
-
-puts JSON.dump(msg)
-puts ''
-puts ''
-puts ''
-puts Oj.dump(msg, mode: :compat)
+pp conn.send_file('test.groovy', {what: 10})
